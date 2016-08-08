12cb805e24b8d1b4c72c9597deaae20e6dc8e166
Traun Leyden <tleyden@couchbase.com>
2016-08-08 15:28:21 -0700

    Finds cloudaccount by upstream account id

--- cloudevent.go
+++ cloudevent.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/goadesign/goa"
 	"github.com/tleyden/zerocloud/app"
+	"github.com/tleyden/zerocloud/models"
 )
 
 // CloudeventController implements the cloudevent resource.
@@ -24,6 +25,30 @@ func (c *CloudeventController) Create(ctx *app.CreateCloudeventContext) error {
 	// Put your logic here
 
 	log.Printf("aws account id: %v", ctx.Payload.AwsAccountID)
+
+	// try to find the CloudAccount that has an upstream_account_id that matches param
+	//  cdb.Db.Model()
+	// rows, err := cdb.Db.Model(&models.CloudAccount{}).Where("upstream_account_id = ?", ctx.Payload.AwsAccountID).Select("id").Rows()
+	cloudAccount := models.CloudAccount{}
+	cdb.Db.Where(&models.CloudAccount{UpstreamAccountID: ctx.Payload.AwsAccountID}).First(&cloudAccount)
+	log.Printf("cloudAccount: %+v", cloudAccount)
+
+	/*
+		if err != nil {
+			log.Printf("Got err: %s", err)
+			return err
+		}
+		log.Printf("iterating over rows")
+		for rows.Next() {
+			log.Printf("row ..")
+			var id string
+			if err := rows.Scan(&id); err != nil {
+				log.Fatal(err)
+			}
+			fmt.Printf("id is %s\n", id)
+		}
+		log.Printf("/iterating over rows")
+	*/
 
 	// CloudeventController_Create: end_implement
 	return nil
