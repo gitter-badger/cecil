0d7ba7130fbfe7b387aa2c83afe9fac1d8f391a6
Traun Leyden <tleyden@couchbase.com>
2016-08-10 11:14:44 -0700

    Adding more fields to CloudEvent (in progress)

--- cloudevent_impl.go
+++ cloudevent_impl.go
@@ -20,29 +20,53 @@ func (c *CloudeventController) CreateImpl(ctx *app.CreateCloudeventContext) erro
 
 	// Make sure we found a valid CloudAccount, otherwise abort
 	if cloudAccount.ID == 0 {
-		ctx.ResponseData.Service.Send(ctx.Context, 400, fmt.Sprintf("Could not find CloudAccount with upstream provider account id: %v", awsAccountId))
+		ctx.ResponseData.Service.Send(
+			ctx.Context,
+			400,
+			fmt.Sprintf("Could not find CloudAccount with upstream provider account id: %v", awsAccountId),
+		)
 		return nil
 	}
 
-	// Get the instance_tags (assumes they have been looked up after pulling from SQS and calling this REST api)
+	/*
+		Field("sqs_payload", gorma.Text)
+		Field("sqs_timestamp", gorma.Timestamp)
+		Field("cw_event_detail_type", gorma.String)
+		Field("cw_event_source", gorma.String)
+		Field("cw_event_timestamp", gorma.Timestamp)
+		Field("cw_event_region", gorma.String)
+		Field("cw_event_detail_instance_id", gorma.String)
+		Field("cw_event_detail_state", gorma.String)
+
+	*/
 
 	// Save the raw CloudEvent to the database
-
-	// Call CloudEvent.createLeaseMaybe()
-	// Or can this be an AfterCreate callback on the CloudEvent?
-	// file:///Users/tleyden/DevLibraries/gorm/callbacks.html
-
-	// Put your logic here
 	e := models.CloudEvent{}
 	e.AwsAccountID = awsAccountId
 	e.CloudAccountID = cloudAccount.ID
 	e.AccountID = cloudAccount.AccountID
-
 	err := edb.Add(ctx.Context, &e)
 	if err != nil {
 		return ErrDatabaseError(err)
 	}
+
+	// Create a Lease object that references this (immutable) CloudEvent and expires
+	// based on the settings in the Account
+	// TODO: or can this be an AfterCreate callback on the CloudEvent?
+	// file:///Users/tleyden/DevLibraries/gorm/callbacks.html
+	err = createLease(e)
+	if err != nil {
+		return ErrDatabaseError(err)
+	}
+
+	// TODO: should this return the path to the cloudevent .. should there even be one?
 	/// ctx.ResponseData.Header().Set("Location", app.CloudeventHref(ctx.AccountID, a.ID))
+
 	return ctx.Created()
 
+}
+
+func createLease(cloudEvent models.CloudEvent) error {
+	// TODO
+	return nil
 }
