72fbb0fac03d68a751aa3aa82c93918ca9dacc75
S <slv.balsan@gmail.com>
2016-09-13 15:37:27 +0200

    move operations from EventInjestorJob() to NewLeaseQueueConsumer

--- core/task-consumers.go
+++ core/task-consumers.go
@@ -20,6 +20,431 @@ import (
 
 func (s *Service) NewLeaseQueueConsumer(t interface{}) error {
 
+	if t == nil {
+		return fmt.Errorf("%v", "t is nil")
+	}
+	transmission := t.(NewLeaseTask).Transmission
+
+	//check whether someone with this aws adminAccount id is registered at zerocloud
+	err := transmission.FetchCloudAccount()
+	if err != nil {
+		// TODO: notify admin; something fishy is going on.
+		logger.Warn("originator is not registered", "AWSID", transmission.Topic.AWSID)
+		return nil // TODO: return an error ???
+	}
+
+	// check whether the cloud account has an admin account
+	err = transmission.FetchAdminAccount()
+	if err != nil {
+		// TODO: notify admin; something fishy is going on.
+		logger.Warn("Error while retrieving admin account", "error", err)
+		return nil // TODO: return an error ???
+	}
+
+	logger.Info("adminAccount",
+		"adminAccount", transmission.AdminAccount,
+	)
+
+	logger.Info("Creating AssumedConfig", "topicRegion", transmission.Topic.Region, "topicAWSID", transmission.Topic.AWSID, "externalID", transmission.CloudAccount.ExternalID)
+
+	err = transmission.CreateAssumedService()
+	if err != nil {
+		// TODO: this might reveal too much to the admin about zerocloud; be selective and cautious
+		s.sendMisconfigurationNotice(err, transmission.AdminAccount.Email)
+		logger.Warn("error while creating assumed service", "error", err)
+		return nil // TODO: return an error ???
+	}
+
+	err = transmission.CreateAssumedEC2Service()
+	if err != nil {
+		// TODO: this might reveal too much to the admin about zerocloud; be selective and cautious
+		s.sendMisconfigurationNotice(err, transmission.AdminAccount.Email)
+		logger.Warn("error while creating ec2 service with assumed service", "error", err)
+		return nil // TODO: return an error ???
+	}
+
+	err = transmission.DescribeInstance()
+	if err != nil {
+		// TODO: this might reveal too much to the admin about zerocloud; be selective and cautious
+		s.sendMisconfigurationNotice(err, transmission.AdminAccount.Email)
+		logger.Warn("error while describing instances", "error", err)
+		return nil // TODO: return an error ???
+	}
+
+	// check whether the instance specified in the event exists on aws
+	if !transmission.InstanceExists() {
+		logger.Warn("Instance does not exist", "instanceID", transmission.Message.Detail.InstanceID)
+		// remove message from queue
+		err := transmission.DeleteMessage()
+		if err != nil {
+			logger.Warn("DeleteMessage", "error", err)
+		}
+		return nil // TODO: return an error ???
+	}
+
+	logger.Info("describeInstances", "response", transmission.describeInstancesResponse)
+
+	err = transmission.FetchInstance()
+	if err != nil {
+		logger.Warn("error while fetching instance description", "error", err)
+		return nil // TODO: return an error ???
+	}
+
+	err = transmission.ComputeInstanceRegion()
+	if err != nil {
+		logger.Warn("error while computing instance region", "error", err)
+		return nil // TODO: return an error ???
+	}
+
+	/// transmission.Message.InstanceID == transmission.Instance.InstanceID
+	//TODO: might this happen?
+
+	/// transmission.Instance.IsTerminated()
+	/// transmission.Message.Delete()
+
+	// if the message signal that an instance has been terminated, create a task
+	// to mark the lease as terminated
+	if transmission.InstanceIsTerminated() {
+
+		s.LeaseTerminatedQueue.TaskQueue <- LeaseTerminatedTask{
+			AWSID:      transmission.CloudAccount.AWSID,
+			InstanceID: *transmission.Instance.InstanceId,
+		}
+
+		// remove message from queue
+		err := transmission.DeleteMessage()
+		if err != nil {
+			logger.Warn("DeleteMessage", "error", err)
+		}
+		return nil // TODO: return an error ???
+	}
+
+	// do not consider states other than pending and terminated
+	if !transmission.InstanceIsPendingOrRunning() {
+		logger.Warn("The retrieved state is neither pending nor running:", "state", transmission.Instance.State.Name)
+		// remove message from queue
+		// remove message from queue
+		err := transmission.DeleteMessage()
+		if err != nil {
+			logger.Warn("DeleteMessage", "error", err)
+		}
+		return nil // TODO: return an error ???
+	}
+
+	if !transmission.LeaseIsNew() {
+		// TODO: notify admin
+		logger.Warn("instanceCount != 0")
+		return nil // TODO: return an error ???
+	}
+
+	if !transmission.InstanceHasGoodOwnerTag() || !transmission.ExternalOwnerIsWhitelisted() {
+		// assign instance to admin, and send notification to admin
+		// owner is not whitelisted: notify admin: "Warning: zerocloudowner tag email not in whitelist"
+
+		err := transmission.SetAdminAsOwner()
+		if err != nil {
+			logger.Warn("Error while setting admin as owner", "error", err)
+		}
+
+		transmission.leaseDuration = time.Duration(ZCDefaultLeaseApprovalTimeoutDuration)
+		var expiresAt = time.Now().UTC().Add(transmission.leaseDuration)
+
+		newLease := Lease{
+			OwnerID:        transmission.owner.ID,
+			CloudAccountID: transmission.CloudAccount.ID,
+			AWSAccountID:   transmission.CloudAccount.AWSID,
+
+			InstanceID:       *transmission.Instance.InstanceId,
+			Region:           transmission.instanceRegion,
+			AvailabilityZone: *transmission.Instance.Placement.AvailabilityZone,
+			InstanceType:     *transmission.Instance.InstanceType,
+
+			// Terminated bool `sql:"DEFAULT:false"`
+			// Deleted    bool `sql:"DEFAULT:false"`
+
+			LaunchedAt: transmission.Instance.LaunchTime.UTC(),
+			ExpiresAt:  expiresAt,
+		}
+		s.DB.Create(&newLease)
+		logger.Info("new lease created",
+			"lease", newLease,
+		)
+
+		var newEmailBody string
+
+		switch {
+		case !transmission.InstanceHasGoodOwnerTag():
+			newEmailBody = compileEmail(
+				`Hey {{.owner_email}}, someone created a new instance 
+				(id <b>{{.instance_id}}</b>, of type <b>{{.instance_type}}</b>, 
+				on <b>{{.instance_region}}</b>). <br><br>
+
+				It does not have a valid ZeroCloudOwner tag, so we assigned it to you.
+				
+				<br>
+				<br>
+				
+				It will be terminated at <b>{{.termination_time}}</b> ({{.instance_duration}} after it's creation).
+
+				<br>
+				<br>
+				
+				Terminate now:
+				<br>
+				<br>
+				{{.instance_terminate_url}}
+
+				<br>
+				<br>
+				Thanks for using ZeroCloud!
+				`,
+
+				map[string]interface{}{
+					"owner_email":     transmission.owner.Email,
+					"instance_id":     *transmission.Instance.InstanceId,
+					"instance_type":   *transmission.Instance.InstanceType,
+					"instance_region": transmission.instanceRegion,
+
+					"termination_time":       expiresAt.Format("2006-01-02 15:04:05 GMT"),
+					"instance_duration":      transmission.leaseDuration.String(),
+					"instance_renew_url":     "",
+					"instance_terminate_url": "",
+				},
+			)
+			break
+
+		case !transmission.ExternalOwnerIsWhitelisted():
+			newEmailBody = compileEmail(
+				`Hey {{.owner_email}}, someone created a new instance 
+				(id <b>{{.instance_id}}</b>, of type <b>{{.instance_type}}</b>, 
+				on <b>{{.instance_region}}</b>). <br><br>
+
+				The ZeroCloudOwner tag of this instance is not in the whitelist, so we assigned it to you.
+				
+				<br>
+				<br>
+				
+				It will be terminated at <b>{{.termination_time}}</b> ({{.instance_duration}} after it's creation).
+
+				<br>
+				<br>
+				
+				Terminate now:
+				<br>
+				<br>
+				{{.instance_terminate_url}}
+
+				<br>
+				<br>
+				Thanks for using ZeroCloud!
+				`,
+
+				map[string]interface{}{
+					"owner_email":     transmission.owner.Email,
+					"instance_id":     *transmission.Instance.InstanceId,
+					"instance_type":   *transmission.Instance.InstanceType,
+					"instance_region": transmission.instanceRegion,
+
+					"termination_time":       expiresAt.Format("2006-01-02 15:04:05 GMT"),
+					"instance_duration":      transmission.leaseDuration.String(),
+					"instance_renew_url":     "",
+					"instance_terminate_url": "",
+				},
+			)
+		}
+
+		s.NotifierQueue.TaskQueue <- NotifierTask{
+			//To:       owner.Email,
+			From:     ZCMailerFromAddress,
+			To:       transmission.AdminAccount.Email,
+			Subject:  fmt.Sprintf("Instance (%v) needs attention", *transmission.Instance.InstanceId),
+			BodyHTML: newEmailBody,
+			BodyText: newEmailBody,
+		}
+
+		err = transmission.DeleteMessage()
+		if err != nil {
+			logger.Warn("DeleteMessage", "error", err)
+		}
+		return nil // TODO: return an error ???
+	}
+
+	err = transmission.SetExternalOwnerAsOwner()
+	if err != nil {
+		logger.Warn("Error while setting external owner as owner", "error", err)
+	}
+
+	if transmission.LeaseNeedsApproval() {
+		// register new lease in DB
+		// expiry: 1h
+		// send confirmation to owner: confirmation link, and termination link
+
+		transmission.leaseDuration = time.Duration(ZCDefaultLeaseApprovalTimeoutDuration)
+		var expiresAt = time.Now().UTC().Add(transmission.leaseDuration)
+
+		newLease := Lease{
+			OwnerID:        transmission.owner.ID,
+			CloudAccountID: transmission.CloudAccount.ID,
+			AWSAccountID:   transmission.CloudAccount.AWSID,
+
+			InstanceID:       *transmission.Instance.InstanceId,
+			Region:           transmission.instanceRegion,
+			AvailabilityZone: *transmission.Instance.Placement.AvailabilityZone,
+
+			// Terminated bool `sql:"DEFAULT:false"`
+			// Deleted    bool `sql:"DEFAULT:false"`
+
+			LaunchedAt:   transmission.Instance.LaunchTime.UTC(),
+			ExpiresAt:    expiresAt,
+			InstanceType: *transmission.Instance.InstanceType,
+		}
+		s.DB.Create(&newLease)
+		logger.Info("new lease created",
+			"lease", newLease,
+		)
+
+		newEmailBody := compileEmail(
+			`Hey {{.owner_email}}, you (or someone else using your ZeroCloudOwner tag) created a new instance 
+				(id <b>{{.instance_id}}</b>, of type <b>{{.instance_type}}</b>, 
+				on <b>{{.instance_region}}</b>). <br><br>
+
+				At the time of writing this email, you have {{.n_of_active_leases}} active
+					leases, so we need your approval for this one. <br><br>
+
+				Please click on "Approve" to approve this instance,
+					otherwise it will be terminated at <b>{{.termination_time}}</b> ({{.instance_duration}} after it's creation).
+
+				<br>
+				<br>
+
+				Approve:
+				<br>
+				<br>
+				{{.instance_renew_url}}
+
+				<br>
+				<br>
+				
+				Terminate:
+				<br>
+				<br>
+				{{.instance_terminate_url}}
+				
+				<br>
+				<br>
+				Thanks for using ZeroCloud!
+				`,
+
+			map[string]interface{}{
+				"owner_email":        transmission.owner.Email,
+				"n_of_active_leases": transmission.activeLeaseCount,
+				"instance_id":        *transmission.Instance.InstanceId,
+				"instance_type":      *transmission.Instance.InstanceType,
+				"instance_region":    transmission.instanceRegion,
+
+				"termination_time":       expiresAt.Format("2006-01-02 15:04:05 GMT"),
+				"instance_renew_url":     "",
+				"instance_terminate_url": "",
+				"instance_duration":      transmission.leaseDuration.String(),
+			},
+		)
+		s.NotifierQueue.TaskQueue <- NotifierTask{
+			From:     ZCMailerFromAddress,
+			To:       transmission.owner.Email,
+			Subject:  fmt.Sprintf("Instance (%v) needs approval", *transmission.Instance.InstanceId),
+			BodyHTML: newEmailBody,
+			BodyText: newEmailBody,
+		}
+
+		// remove message from queue
+		err = transmission.DeleteMessage()
+		if err != nil {
+			logger.Warn("DeleteMessage", "error", err)
+		}
+		return nil // TODO: return an error ???
+	} else {
+		// register new lease in DB
+		// set its expiration to zone.default_expiration (if > 0), or cloudAccount.default_expiration, or adminAccount.default_expiration
+
+		transmission.DefineLeaseDuration()
+		var expiresAt = time.Now().UTC().Add(transmission.leaseDuration)
+
+		newLease := Lease{
+			OwnerID:        transmission.owner.ID,
+			CloudAccountID: transmission.CloudAccount.ID,
+			AWSAccountID:   transmission.CloudAccount.AWSID,
+
+			InstanceID:       *transmission.Instance.InstanceId,
+			Region:           transmission.instanceRegion,
+			AvailabilityZone: *transmission.Instance.Placement.AvailabilityZone,
+
+			// Terminated bool `sql:"DEFAULT:false"`
+			// Deleted    bool `sql:"DEFAULT:false"`
+
+			LaunchedAt:   transmission.Instance.LaunchTime.UTC(),
+			ExpiresAt:    expiresAt,
+			InstanceType: *transmission.Instance.InstanceType,
+		}
+		s.DB.Create(&newLease)
+		logger.Info("new lease created",
+			"lease", newLease,
+		)
+
+		newEmailBody := compileEmail(
+			`Hey {{.owner_email}}, you (or someone else using your ZeroCloudOwner tag) created a new instance 
+				(id <b>{{.instance_id}}</b>, of type <b>{{.instance_type}}</b>, 
+				on <b>{{.instance_region}}</b>). That's AWESOME!
+
+				<br>
+				<br>
+
+				Your instance will be terminated at <b>{{.termination_time}}</b> ({{.instance_duration}} after it's creation).
+
+				<br>
+				<br>
+				
+				Thanks for using ZeroCloud!
+				`,
+
+			map[string]interface{}{
+				"owner_email":     transmission.owner.Email,
+				"instance_id":     *transmission.Instance.InstanceId,
+				"instance_type":   *transmission.Instance.InstanceType,
+				"instance_region": transmission.instanceRegion,
+
+				"termination_time":  expiresAt.Format("2006-01-02 15:04:05 GMT"),
+				"instance_duration": transmission.leaseDuration.String(),
+			},
+		)
+		s.NotifierQueue.TaskQueue <- NotifierTask{
+			From:     ZCMailerFromAddress,
+			To:       transmission.owner.Email,
+			Subject:  fmt.Sprintf("Instance (%v) created", *transmission.Instance.InstanceId),
+			BodyHTML: newEmailBody,
+			BodyText: newEmailBody,
+		}
+
+		// remove message from queue
+		err = transmission.DeleteMessage()
+		if err != nil {
+			logger.Warn("DeleteMessage", "error", err)
+		}
+		return nil // TODO: return an error ???
+	}
+
+	// if message.Detail.State == ec2.InstanceStateNameTerminated
+	// LeaseTerminatedQueue <- LeaseTerminatedTask{} and continue
+
+	// get zc adminAccount who has a cloudaccount with awsID == topicAWSID
+	// if no one of our customers owns this adminAccount, error
+	// fetch options config
+	// roleARN := fmt.Sprintf("arn:aws:iam::%v:role/ZeroCloudRole",topicAWSID)
+	// assume role
+	// fetch instance info
+	// check if statuses match (this message was sent by aws.ec2)
+	// message.Detail.InstanceID
+
+	// fmt.Printf("%v", message)
 	return nil
 }
 
--- core/task-structs.go
+++ core/task-structs.go
@@ -1,20 +1,11 @@
 package core
 
-import (
-	"time"
-)
+import ()
 
 // @@@@@@@@@@@@@@@ Task structs @@@@@@@@@@@@@@@
 
 type NewLeaseTask struct {
-	AWSAccountID string // message.account
-	InstanceID   string // message.detail.instance-id
-	Region       string // message.region
-
-	LaunchedAt    time.Time // get from the request for tags to ec2 api, not from event
-	InstanceType  string
-	InstanceOwner string
-	//InstanceTags []string
+	*Transmission
 }
 
 type TerminatorTask struct {
