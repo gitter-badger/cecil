4c945d8a79a477ff5e658fca8b50ac37fa5f4515
S <slv.balsan@gmail.com>
2016-10-24 11:56:18 +0200

    Stub out task structs Validate() methods

--- core/task-structs.go
+++ core/task-structs.go
@@ -4,11 +4,18 @@ import (
 	"time"
 )
 
-// @@@@@@@@@@@@@@@ Task structs @@@@@@@@@@@@@@@
+// @@@@@@@@@@@@@@@ Notifier task @@@@@@@@@@@@@@@
 
 type NewLeaseTask struct {
 	*Transmission
 }
+
+func (t *NewLeaseTask) Validate() error {
+
+	return nil
+}
+
+// @@@@@@@@@@@@@@@ Terminator task @@@@@@@@@@@@@@@
 
 type TerminatorTask struct {
 	Lease
@@ -16,11 +23,25 @@ type TerminatorTask struct {
 	Action string // default is TerminatorActionTerminate
 }
 
+func (t *TerminatorTask) Validate() error {
+
+	return nil
+}
+
+// @@@@@@@@@@@@@@@ Lease terminated task @@@@@@@@@@@@@@@
+
 type LeaseTerminatedTask struct {
 	AWSID        string
 	InstanceID   string
 	TerminatedAt time.Time
 }
+
+func (t *LeaseTerminatedTask) Validate() error {
+
+	return nil
+}
+
+// @@@@@@@@@@@@@@@ Extender task @@@@@@@@@@@@@@@
 
 type ExtenderTask struct {
 	Lease
@@ -29,10 +50,22 @@ type ExtenderTask struct {
 	Approving bool
 }
 
+func (t *ExtenderTask) Validate() error {
+
+	return nil
+}
+
+// @@@@@@@@@@@@@@@ Notifier task @@@@@@@@@@@@@@@
+
 type NotifierTask struct {
 	From     string
 	To       string
 	Subject  string
 	BodyHTML string
 	BodyText string
+}
+
+func (t *NotifierTask) Validate() error {
+
+	return nil
 }
