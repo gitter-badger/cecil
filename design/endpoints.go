667dbbb4ed21c32d46a031f3a491c49bb88229f6
S <slv.balsan@gmail.com>
2016-11-06 18:32:17 +0100

    Add aditional parameter validation; add goagen.sh

--- design/endpoints.go
+++ design/endpoints.go
@@ -36,7 +36,11 @@ var _ = Resource("account", func() {
 
 		Routing(POST("/:account_id/api_token"))
 		Params(func() {
-			Param("account_id", Integer, "Account Id")
+			Param("account_id", Integer, "Account Id",
+				func() {
+					Minimum(1)
+				},
+			)
 		})
 		Payload(AccountVerificationInputPayload, func() {
 			Required("verification_token")
@@ -46,8 +50,15 @@ var _ = Resource("account", func() {
 	})
 
 	Action("show", func() {
-		Routing(GET("/:account_id"))
 		Description("Show account")
+		Routing(GET("/:account_id"))
+		Params(func() {
+			Param("account_id", Integer, "Account Id",
+				func() {
+					Minimum(1)
+				},
+			)
+		})
 		Response(OK)
 	})
 
@@ -75,7 +86,11 @@ var _ = Resource("cloudaccount", func() {
 		Description("Add new email to owner tag whitelist")
 		Routing(POST("/:cloudaccount_id/owners"))
 		Params(func() {
-			Param("cloudaccount_id", Integer, "CloudAccount Id")
+			Param("cloudaccount_id", Integer, "CloudAccount Id",
+				func() {
+					Minimum(1)
+				},
+			)
 		})
 		Payload(OwnerInputPayload, func() {
 			Required("email")
@@ -87,7 +102,11 @@ var _ = Resource("cloudaccount", func() {
 		Description("Download AWS initial setup cloudformation template")
 		Routing(GET("/:cloudaccount_id/cecil-aws-initial-setup.template"))
 		Params(func() {
-			Param("cloudaccount_id", Integer, "CloudAccount Id")
+			Param("cloudaccount_id", Integer, "CloudAccount Id",
+				func() {
+					Minimum(1)
+				},
+			)
 		})
 		Response(OK)
 	})
@@ -96,7 +115,11 @@ var _ = Resource("cloudaccount", func() {
 		Description("Download AWS region setup cloudformation template")
 		Routing(GET("/:cloudaccount_id/cecil-aws-region-setup.template"))
 		Params(func() {
-			Param("cloudaccount_id", Integer, "CloudAccount Id")
+			Param("cloudaccount_id", Integer, "CloudAccount Id",
+				func() {
+					Minimum(1)
+				},
+			)
 		})
 		Response(OK)
 	})
@@ -111,13 +134,24 @@ var _ = Resource("email_action", func() {
 		Routing(GET("/leases/:lease_uuid/:instance_id/:action"))
 		Params(func() {
 			Param("lease_uuid", UUID, "UUID of the lease")
-			Param("instance_id", String, "ID of the lease")
+			Param("instance_id", String, "ID of the lease",
+				func() {
+					MinLength(1)
+				},
+			)
 			Param("action", String, "Action to be peformed on the lease", func() {
 				Enum("approve", "terminate", "extend")
 			})
-			Param("tok", String, "The token_once of this link")
-			Param("sig", String, "The signature of this link")
-			Required("tok", "sig")
+			Param("tok", String, "The token_once of this link",
+				func() {
+					MinLength(30)
+				},
+			)
+			Param("sig", String, "The signature of this link",
+				func() {
+					MinLength(30)
+				})
+			Required("lease_uuid", "instance_id", "action", "tok", "sig")
 		})
 		Response(OK)
 	})
---
+++ goagen.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# generate API in the ./goa folder
+echo "Generating API code with Goagen..."
+$GOPATH/bin/goagen bootstrap -d github.com/tleyden/zerocloud/design -o ./goa
+echo -e "\n"
+echo    "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+echo    "@@@ API code generated in ./goa folder @@@"
+echo    "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+echo -e "\n"
