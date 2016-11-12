5ecf746f12b9adb341c7c718e4925460184ade48
Traun Leyden <traun.leyden@gmail.com>
2016-11-11 21:00:32 -0800

    Fail script if any commands fail

--- goagen.sh
+++ goagen.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+set -e
+set -x
+
 # generate API in the ./goa folder
 echo "Generating API code with Goagen..."
 $GOPATH/bin/goagen bootstrap -d github.com/tleyden/zerocloud/design -o ./goa
